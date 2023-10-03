5f9cb4a0a98c23f12d9351fee71877f69cbf45e6
Anushasri-27 <anushasrivastava1999@gmail.com>
2023-10-03 16:54:41 +0530

    first commit

---
+++ Home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+    <link href="./styles//home.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
+</head>
+
+<body>
+    <header>
+        <?php require_once "Menu.php" ?>
+
+    </header>
+    <div class="register-wrap">
+        <div  class="register-agent">
+            <?php require_once "./views/Register_agent.php" ?>
+        </div>
+        <div class="register-cust">
+            <?php require_once "./views/Register_cust.php" ?>
+
+        </div>
+
+    </div>
+
+    <footer>
+        <div class="footer">footer</div>
+
+    </footer>
+
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
